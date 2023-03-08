5526b8675939d3e773be2450c82da2a6b9b526a7
eliabev <eliabev@gmail.com>
2023-03-08 07:37:13 -0300

    Add first modules and use express

---
+++ index.js
@@ -0,0 +1,17 @@
+// current https://www.youtube.com/watch?v=K5QaTfE5ylk&t=345s
+const express = require('express')
+const app = express()
+
+// way to read/parse JSON
+// middlewares are resources executed between req and response
+app.use( // create middleware
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// initial route
+
+// delivery a port
